1877f77e09e8a49a35a06047a3f14b3a0120e5ac
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-25 20:36:25 -0400

    Creating a Simple Web Server

--- index.js
+++ index.js
@@ -1,5 +1,11 @@
 // Blocking, synchronous way
 const fs = require("fs");
+
+//----------------------------------------------------------
+// FILES
+const http = require("http");
+//----------------------------------------------------------
+
 // const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
 // console.log(textIn);
 // const textOut = `This is what we know about the avocado: ${textIn}.\nCreated on ${Date.now()}`;
@@ -7,18 +13,30 @@ const fs = require("fs");
 // console.log("File written");
 
 // Non-blocking, asynchronous way
-fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
-  if (err) return console.log("ERROR!");
+// fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+//   if (err) return console.log("ERROR!");
 
-  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
-    console.log(data2);
-    fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
-      console.log(data3);
+//   fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+//     console.log(data2);
+//     fs.readFile("./txt/append.txt", "utf-8", (err, data3) => {
+//       console.log(data3);
 
-      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
-        console.log("Your file has been written :)");
-      });
-    });
-  });
+//       fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
+//         console.log("Your file has been written :)");
+//       });
+//     });
+//   });
+// });
+// console.log("Will read file!");
+
+//----------------------------------------------------------
+// SERVER
+const server = http.createServer((req, res) => {
+  //   console.log(req);
+  res.end("Hello from the server!");
 });
-console.log("Will read file!");
+
+server.listen(8000, "127.0.0.1");
+console.log("listening to request on port 8000");
+
+//----------------------------------------------------------
