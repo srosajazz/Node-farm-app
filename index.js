3ee7fca4c5ace9b7ffefcfb70bb1f81d5877a20a
Sergio Rosa <scarlosjazz@gmail.com>
2019-06-27 09:02:07 -0400

    Project Complete

--- index.js
+++ index.js
@@ -5,6 +5,8 @@
 const fs = require("fs");
 const http = require("http");
 const url = require("url");
+const slugify = require("slugify");
+
 const replaceTemplate = require("./modules/replaceTemplate");
 
 //----------------------------------------------------------
@@ -50,6 +52,8 @@ const tempProduct = fs.readFileSync(
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, "utf-8");
 const dataObj = JSON.parse(data);
+
+console.log(slugify("Fresh Avocados", { lower: true }));
 
 const server = http.createServer((req, res) => {
   const { query, pathname } = url.parse(req.url, true);
